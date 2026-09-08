e6d66c32725b39f85191c59b0270cfee6202344a
Александр Прокопчук <aleksaleks39616@gmail.com>
2026-09-08 22:20:54 +0300

    исправления

--- js/functions.js
+++ js/functions.js
@@ -6,7 +6,7 @@ checkStringLength('Мой проект', 20);
 
 // Функция проверки на палиндром:
 function checkPalindrom (string) {
-let cleanded = string.replaceAll(' ', '').toLowerCase();
+let cleaned = string.replaceAll(' ', '').toLowerCase();
 let reversed = '';
 for (let i = cleaned.length - 1; i >= 0; i--) {
     reversed += cleaned[i];
